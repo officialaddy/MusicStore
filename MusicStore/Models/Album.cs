80f8a371af5057a97ae3f966cdc566e6f313f2b5
Aditya <officialaddy2337@gmail.com>
2020-04-12 06:48:22 +0300

    Album Model Created

---
+++ MusicStore/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
